53df576982b66fcf113213f66b336d2b435ae607
Diego Iglesias <diegoglesiasgodoy@gmail.com>
2025-09-07 18:34:47 -0400

    redireccion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-blue-100">
-<h1 className="text-4xl font-bold text-blue-600">¡Tailwind funciona! 🚀</h1>
-    </main>
-  );
+  redirect('/chat') // ⬅️ redirige a /chat
 }
